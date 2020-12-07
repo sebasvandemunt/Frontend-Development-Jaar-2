5c87578eb0bbdb88e3016179afe2950b9ac41153
sebasvandemunt <56074252+sebasvandemunt@users.noreply.github.com>
2020-12-07 13:24:52 +0100

    Gaan we weer vol gas
    Onnodige documenten verwijderd. Hamburger menu gebouwd met HTML/ CSS/ Javascript. Header balk bestaat maar heeft nog geen inhoud

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
 // Javascript Document
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
